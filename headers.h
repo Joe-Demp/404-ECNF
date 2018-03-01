60596993b0823408803f737f072c9a5ad9f037fd
Conor Dunne <36004055+dunne-conor@users.noreply.github.com>
2018-03-01 19:50:33 +0000

    Update headers.h

--- headers.h
+++ headers.h
@@ -1,6 +1,3 @@
-/*All headers for project
- *Please edit this file on your own branch before committing to master 
- *		and resolve any merge conflicts
- */
-
 int distance(int x1, int y1, int x2, int y2);
+
+void nextPassenger(int startTimes[], int startTimeID[], int N);
