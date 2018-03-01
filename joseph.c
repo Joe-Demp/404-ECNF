cd97876d461090fb3677f50ded4b04f2c655f759
Conor Dunne <36004055+dunne-conor@users.noreply.github.com>
2018-03-01 18:56:39 +0000

    Update joseph.c

--- joseph.c
+++ joseph.c
@@ -2,18 +2,29 @@
 #include <stdlib.h>
 #include "headers.h"
 
-/*Functions created by Joe for the problem*/
-int main(void){
-	int d;
-	d = distance(0, 0, 1, 1);
-	printf("%d\n", d);
-	return 0;
-}
-
 /*Function to calculate the distance between two points (x1, y1) and (x2, y2) on the city map*/
 int distance(int x1, int y1, int x2, int y2){
 	int d = 0;
 	d += abs(x1-x2);
 	d += abs(y1-y2);
 	return d;
+}
+
+int firstPassenger(int s[], int N)
+{
+    int min = s[0];
+    int index = 0;
+    int counter = 0;
+
+    for(int i=1; i<N; i++)
+    {
+        if(s[i] < min)
+        {
+            min = i;
+            index = i;
+            counter = 0;
+        }
+        else if(s[i] == min)
+            counter ++;
+    }
 }
