b910d770ff118fdd4e5dc289d781f60e1c28046c
Conor Dunne <36004055+dunne-conor@users.noreply.github.com>
2018-03-01 19:04:24 +0000

    Update joseph.c

--- joseph.c
+++ joseph.c
@@ -2,18 +2,35 @@
 #include <stdlib.h>
 #include "headers.h"
 
-/*Functions created by Joe for the problem*/
-int main(void){
-	int d;
-	d = distance(0, 0, 1, 1);
-	printf("%d\n", d);
-	return 0;
-}
-
 /*Function to calculate the distance between two points (x1, y1) and (x2, y2) on the city map*/
 int distance(int x1, int y1, int x2, int y2){
 	int d = 0;
 	d += abs(x1-x2);
 	d += abs(y1-y2);
 	return d;
+}
+
+//	Function to decide the Next Passanger to Pick Up
+int nextPassenger(int s[], int N)
+{
+    int swap;
+    int ID[N];
+
+    for(int i=0; i<N; i++)
+        ID = i;
+    for(int i=1; i<N; i++)
+    {
+        j = i;
+
+        while(j>0 && s[j] < s[j-1])
+        {
+            swap = s[j];
+            s[j] = s[i];
+            s[i] = swap;
+
+            swap = ID[j];
+            ID[j] = ID[i];
+            ID[i] = swap;
+        }
+    }
 }
